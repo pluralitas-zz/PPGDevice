ca3c63259009c819a465eb6eb34cec424c9a6a5e
pluralitas <aucheukyan@gmail.com>
2020-03-15 02:24:13 +0800

    Update PPGSerial.py
    * Shorten code, reduce amount of variables

--- PPGSerial.py
+++ PPGSerial.py
@@ -4,56 +4,43 @@ from numpy import mean
 from PyQt5.QtCore import pyqtSignal, QThread
 
 class Arduino(QThread):
-    _heartbeat = pyqtSignal(int)
-    _heartrate = pyqtSignal(int)
-
-    comports=list()
-    comnames=list()
-    beatsArr = list()
+    _heartbeat = pyqtSignal(int) # if emit 1, means heart beat, if emit between 30-200 means heart rate
+    beatsArr=list() #for storing time between heart beat
 
     def __init__(self):
-        ports = serial.tools.list_ports.comports() #find all coms
-        for p in ports: 
-            self.comports.append(p[0])
-            self.comnames.append(p[1])
-
-        for i in range(len(self.comnames)):
-            if "Silicon Labs CP210x USB to UART Bridge" in self.comnames[i]:
-                self.comport = self.comports[i]
-        
-        if self.comport != []:
-            self.com = serial.Serial(port=self.comport,baudrate=38400,parity=serial.PARITY_NONE,stopbits=serial.STOPBITS_ONE,bytesize=serial.EIGHTBITS,rtscts=True)
-        else:
-            raise Exception("Please plug in the PPG device")
+        ports = list(serial.tools.list_ports.comports()) #find all coms
+        for comnum, comname, comadd in ports:  #find comport of seeeduino nano
+            if "Silicon Labs CP210x USB to UART Bridge" in comname:
+                self.com = serial.Serial(port=comnum,baudrate=38400,parity=serial.PARITY_NONE,stopbits=serial.STOPBITS_ONE,bytesize=serial.EIGHTBITS,rtscts=True)
+        try: #see if serial is connected and open
+            self.com.name
+        except:
+            raise Exception("Please plug in the PPG device.")
 
     @property
-    def acqhb(self): #acquire Heartbeat from Seeeduino Nano
+    def acqhb(self): #acquire Heart beat from Seeeduino Nano
         self.com.flushInput()
         self.raw = self.com.readline()
         self.com.flushInput()
-        self.heartbeat = int(self.raw[:-2].decode())
-        return self.heartbeat
+        return int(self.raw[:-2].decode())
 
-    def acqhr(self,hb):
+    def acqhr(self,hb): #calculate and emit Heart Rate
         if hb > 0:
             self.beatsArr.append(hb)
         if len(self.beatsArr) == 5: #use 5 beats average for heart rate
-            self.timemean = int(60/(mean(self.beatsArr)/1000))
             try:
-                self._heartrate.emit(self.timemean) #emit heartrate
+                self._heartbeat.emit(int(60/(mean(self.beatsArr)/1000))) #emit heartrate, value 30-200
             except: 
-                print(self.timemean)
+                print(int(60/(mean(self.beatsArr)/1000)))
             self.beatsArr.clear()
-        else:
-            return 
 
-    def run(self):
+    def run(self): #QThread run
         while True:
             self.hb = self.acqhb
             try:
-                self._heartbeat.emit(1) #emit heartbeat
+                self._heartbeat.emit(1) #emit heartbeat, value=1
             except: pass
-            self.heartrate = self.acqhr(self.hb)
+            self.acqhr(self.hb)
                 
 if __name__ == "__main__":
     ard = Arduino()
